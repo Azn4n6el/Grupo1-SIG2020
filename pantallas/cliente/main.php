aeff332f0ee076f1448fe9286eb0675319e9c0e1
Angel Lam <anglam08@gmail.com>
2020-11-08 14:31:24 -0500

    Pantallas del Sistema
    creando las pantallas principales del sistema

---
+++ pantallas/cliente/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header-clientes">
+        <div class="logo">
+            <img src="#" alt="logoEmpresa" srcset="">
+        </div>
+        <div class="sucursal-button">
+            <a href="#"><img src="#" alt="">Sucursal</a>
+        </div>
+        <div class="productos-button">
+            <a href="#"><img src="#" alt="">Productos</a>
+        </div>
+        <div class="carrito-button">
+            <a href="#"><img src="#" alt="">Carrito</a>
+        </div>
+    </header>
+</body>
+
+</html>
