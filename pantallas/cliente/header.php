05f9cd4a519987c9d55dae1ca16374c971d83422
Angel Lam <anglam08@gmail.com>
2020-11-08 16:48:04 -0500

    creacion-header

---
+++ pantallas/cliente/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/clientes.css">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+    <header class="header-clientes">
+        <div class="left-side-header">
+            <div class="logo">
+                <img src="../../images/super instant1.png" alt="logoEmpresa" width=150>
+            </div>
+            <div class="sucursal-button">
+                <a class="links-image" href="#"><img src="../../images/noun_Store_980053.svg" alt="imgSucursal" width=60>SUCURSAL</a>
+            </div>
+            <div class="productos-button">
+                <a class="links-image" href="#"><img src="../../images/noun_product_1375596.svg" alt="imgProducto" width="60">PRODUCTOS</a>
+            </div>
+        </div>
+        <div class="carrito-button">
+        <a class="links-image" href="#"><img src="../../images/noun_Shopping Bag_2425846.svg" alt="imgProducto" width="60">CARRITO</a>
+        </div>
+    </header>
+</body>
+
+</html>
--- pantallas/cliente/main.php
+++ pantallas/cliente/main.php
@@ -5,23 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../css/clientes.css">
+    <link rel="stylesheet" href="../../css/styles.css">
 </head>
 
 <body>
-    <header class="header-clientes">
-        <div class="logo">
-            <img src="#" alt="logoEmpresa" srcset="">
-        </div>
-        <div class="sucursal-button">
-            <a href="#"><img src="#" alt="">Sucursal</a>
-        </div>
-        <div class="productos-button">
-            <a href="#"><img src="#" alt="">Productos</a>
-        </div>
-        <div class="carrito-button">
-            <a href="#"><img src="#" alt="">Carrito</a>
-        </div>
-    </header>
+    <?php require('header.php');?>
+    
 </body>
 
 </html>
