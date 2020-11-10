e04af7b78db3ab727f8a28ef0e03ebc9bedf8329
Angel Lam <anglam08@gmail.com>
2020-11-10 15:59:07 -0500

    diseño de la pantalla de login

---
+++ pantallas/administrador/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../css/administrador.css">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body class="login-body-background">
+    <div class="login-header">
+        <div class="images-container">
+            <img src="../../images/mujercompra.png" alt="" srcset="">
+            <img src="../../images/basket.png" alt="basket" srcset="" class="basket-image">
+        </div>
+        <form action="" class="login-form">
+            <div class="form-header-container">
+                <div class="form-circle">
+                    <div class="form-circle2">
+                        <div class="form-rope"></div>
+                    </div>
+                </div>
+                <div class="form-title">
+                    Centro Logístico
+                </div>
+                <div class="form-circle">
+                    <div class="form-circle2">
+                        <div class="form-rope"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="login-title">
+                Inicio de Sesión
+            </div>
+            <div class="form-input">
+                <label for="usuario">Usuario:</label>
+                <input type="text" id="usuario" name="usuario" class="input-text" autofocus>
+            </div>
+            <div class="form-input">
+                <label for="passwd">Contraseña:</label>
+                <input type="password" id="passwd" name="passwd" class="input-text">
+            </div>
+            <div class="login-button">
+                <input type="submit" value="Iniciar" class="sign-button">
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
