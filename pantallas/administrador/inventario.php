eb5f44e373d59aa030bcd4cae3091ce97382b9f8
Angel Lam <anglam08@gmail.com>
2020-11-11 08:06:42 -0500

    added animations and added structure

---
+++ pantallas/administrador/inventario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario</title>
+    <link rel="stylesheet" href="../../css/administrador.css">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+    <div class="dashboard-container">
+        <?php require 'dashboard-nav.php' ?>
+        <div class="dashboard-content">
+            <?php require 'dashboard-header.php' ?>
+        </div>
+    </div>
+</body>
+
+</html>
+<script>
+    let links = document.getElementsByClassName('list-links');
+    links[2].style.cssText = 'background-color:var(--form-color); transform:scale(1.1);'
+</script>
