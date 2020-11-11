12ad6d5bfd7e6ba620768727000c4d8928164506
Kiriam Paredes <41593879+Paredes02@users.noreply.github.com>
2020-11-11 12:03:02 -0500

    Pantalla Elegir Sucrusal
    Se realizo el diseño de la pantalla

--- pantallas/cliente/elegirSucursal.php
+++ pantallas/cliente/elegirSucursal.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sucursal</title>
+    <link rel="stylesheet" href="../../css/clientes.css">
+    <link rel="stylesheet" href="../../css/styles.css">
 </head>
+    <header class="header-sucursal" >
+        <div class="left-side-header">
+            <div class="logo">
+                <a href="main.php"><img src="../../images/super instant1.png" alt="logoEmpresa" width="150"></a>
+             </div>
+    </header>
 <body>
-    holaaa
+    <div class="left-side">
+            <div class="image-container">
+                <img src="../../images/cajero.png" alt="cashier" width="600">
+            </div>
+    <div>
+    <div class="right-side">
+        <div class="circle">
+                <div class="sucursal-info">
+                    <h3>Bienvenido!</h3>
+                    <h4><label for="select-sucursal">Elegir Sucursal:</label></h4>
+                    <select class="select" id="select-sucursal">
+                        <option value="Option 1">Tocumen</option>
+                        <option value="Option 2">El Crisol</option>
+                        <option value="Option 3">El Dorado</option>
+                        <option value="Option 4">Via España</option>
+                        <option value="Option 5">Tumba Muerto</option>
+                    </select>
+                    <br>
+                     <input type="submit" value="Siguiente" class="next-button">
+                        
+                </div>   
+        </div>  
+    </div>  
+   
 </body>
 </html>
+
